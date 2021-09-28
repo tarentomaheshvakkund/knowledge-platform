5949673c43d57134522d6a94671c73637082f009
Amit Kumar Yadav <36166686+amit-kumaryadav@users.noreply.github.com>
2021-09-28 09:46:45 +0530

    Import issue.

--- content-api/content-actors/src/main/scala/org/sunbird/content/actors/ContentActor.scala
+++ content-api/content-actors/src/main/scala/org/sunbird/content/actors/ContentActor.scala
@@ -16,7 +16,6 @@ import org.sunbird.common.{ContentParams, Platform, Slug}
 import org.sunbird.common.dto.{Request, Response, ResponseHandler}
 import org.sunbird.common.exception.ClientException
 import org.sunbird.content.dial.DIALManager
-import org.sunbird.content.mgr.ImportManager
 import org.sunbird.util.RequestUtil
 import org.sunbird.content.upload.mgr.UploadManager
 import org.sunbird.graph.OntologyEngineContext
