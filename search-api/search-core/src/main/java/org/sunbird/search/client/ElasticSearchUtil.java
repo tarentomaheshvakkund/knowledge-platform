8ca02700443e4e325ea71b71c224631f7238012e
karthik-tarento <karthikeyan.rajendran@tarento.com>
2022-01-27 11:28:39 +0530

    Fix for search API issue

--- search-api/search-core/src/main/java/org/sunbird/search/client/ElasticSearchUtil.java
+++ search-api/search-core/src/main/java/org/sunbird/search/client/ElasticSearchUtil.java
@@ -620,9 +620,16 @@ public class ElasticSearchUtil {
 			for (Map<String, Object> aggregationsMap : groupByList) {
 				Map<String, Object> parentCountMap = new HashMap<String, Object>();
 				String groupByParent = (String) aggregationsMap.get("groupByParent");
-				Terms terms = aggregations.get(groupByParent);
+				Terms terms = null;
+                List<Bucket> buckets = null;
+                if (groupByParent.contains(".")) {
+	            	Nested nested = aggregations.get(groupByParent.split("\\.")[0]);
+                    terms = nested.getAggregations().get(groupByParent.split("\\.")[1]);
+                } else {
+                    terms = aggregations.get(groupByParent);
+                }
+				buckets = (List<Bucket>)terms.getBuckets();
 				List<Map<String, Object>> parentGroupList = new ArrayList<Map<String, Object>>();
-				List<Bucket> buckets = (List<Bucket>) terms.getBuckets();
 				for (Bucket bucket : buckets) {
 					Map<String, Object> parentCountObject = new HashMap<String, Object>();
 					parentCountObject.put("count", bucket.getDocCount());
