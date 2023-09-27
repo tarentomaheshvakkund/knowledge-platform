a0301678c92c77e968a9dbc5e3c4610427eb7cb7
sreeragksgh <sreeragsajeesh@gmail.com>
2023-09-27 16:04:02 +0530

    logs to analyse hierarchy issue

--- ontology-engine/graph-core_2.11/src/main/scala/org/sunbird/graph/external/store/ExternalStore.scala
+++ ontology-engine/graph-core_2.11/src/main/scala/org/sunbird/graph/external/store/ExternalStore.scala
@@ -3,10 +3,10 @@ package org.sunbird.graph.external.store
 import java.sql.Timestamp
 import java.util
 import java.util.Date
-
 import com.datastax.driver.core.Session
 import com.datastax.driver.core.querybuilder.{Clause, Insert, QueryBuilder}
 import com.google.common.util.concurrent.{FutureCallback, Futures, ListenableFuture, MoreExecutors}
+import org.slf4j.LoggerFactory
 import org.sunbird.cassandra.{CassandraConnector, CassandraStore}
 import org.sunbird.common.JsonUtils
 import org.sunbird.common.dto.{Response, ResponseHandler}
@@ -17,6 +17,7 @@ import scala.concurrent.{ExecutionContext, Future, Promise}
 
 class ExternalStore(keySpace: String , table: String , primaryKey: java.util.List[String]) extends CassandraStore(keySpace, table, primaryKey) {
 
+    val logger = LoggerFactory.getLogger("org.sunbird.graph.schema.validator.FrameworkValidator")
     def insert(request: util.Map[String, AnyRef], propsMapping: Map[String, String])(implicit ec: ExecutionContext): Future[Response] = {
         val insertQuery: Insert = QueryBuilder.insertInto(keySpace, table)
         val identifier = request.get("identifier")
@@ -169,6 +170,8 @@ class ExternalStore(keySpace: String , table: String , primaryKey: java.util.Lis
                 case _ => update.`with`(QueryBuilder.set(column, values(index)))
             }
         }
+        val updateQuery = update.toString
+        logger.info("Query for update hierarchy is: {}", updateQuery)
         try {
             val session: Session = CassandraConnector.getSession
             session.executeAsync(update).asScala.map( resultset => {
