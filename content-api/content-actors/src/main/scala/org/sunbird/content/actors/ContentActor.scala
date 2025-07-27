5f756d3cbe15a9542f00add30f3af10738016fb1
karthik-tarento <karthikeyan.rajendran@tarento.com>
2025-07-27 16:43:32 +0530

    Fixed typo mistake

--- content-api/content-actors/src/main/scala/org/sunbird/content/actors/ContentActor.scala
+++ content-api/content-actors/src/main/scala/org/sunbird/content/actors/ContentActor.scala
@@ -500,7 +500,7 @@ class ContentActor @Inject() (implicit oec: OntologyEngineContext, ss: StorageSe
 		DataNode.read(readRequest).flatMap { node =>
 			val metadata = node.getMetadata
 			val status = metadata.getOrDefault("status", "").asInstanceOf[String]
-			val name = metaData.getOrDefault("name", "").asInstanceOf[String]
+			val name = metadata.getOrDefault("name", "").asInstanceOf[String]
 
 			if (!StringUtils.equalsIgnoreCase(status, "Live"))
 				throw new ClientException("ERR_INVALID_CONTENT_STATUS", s"Content $sourceCollectionId must be in Live status")
