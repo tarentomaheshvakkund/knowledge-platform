d2d4400545e2494e4c562412a4f2a34696a7e349
karthik-tarento <karthikeyan.rajendran@tarento.com>
2025-07-28 12:19:37 +0530

    Added logic to update languageMapV1 upon discarding course

--- content-api/content-actors/src/main/scala/org/sunbird/content/util/DiscardManager.scala
+++ content-api/content-actors/src/main/scala/org/sunbird/content/util/DiscardManager.scala
@@ -29,14 +29,22 @@ object DiscardManager {
             request.put(ContentConstants.IDENTIFIER, node.getIdentifier)
             if (!CONTENT_DISCARD_STATUS.contains(node.getMetadata.get(ContentConstants.STATUS)))
                 throw new ClientException(ContentConstants.ERR_CONTENT_NOT_DRAFT, "No changes to discard for content with content id: " + node.getIdentifier + " since content status isn't draft", node.getIdentifier)
-            val response = if (StringUtils.equalsIgnoreCase(node.getMetadata.getOrDefault(ContentConstants.MIME_TYPE, "").asInstanceOf[String], ContentConstants.COLLECTION_MIME_TYPE))
-                discardForCollection(node, request)
-            else
-                DataNode.deleteNode(request)
-            response.map(resp => 		{
-              ResponseHandler.OK().put("node_id", node.getIdentifier).put("identifier", node.getIdentifier)
-                  .put("message", "Draft version of the content with id : " + node.getIdentifier + " is discarded")
-            })
+            cleanLanguageReferences(node.getIdentifier).flatMap { _ =>
+                val discardOp =
+                    if (StringUtils.equalsIgnoreCase(
+                        node.getMetadata.getOrDefault(ContentConstants.MIME_TYPE, "").asInstanceOf[String],
+                        ContentConstants.COLLECTION_MIME_TYPE))
+                    discardForCollection(node, request)
+                    else
+                    DataNode.deleteNode(request)
+
+                discardOp.map { _ =>
+                    ResponseHandler.OK()
+                    .put("node_id", node.getIdentifier)
+                    .put("identifier", node.getIdentifier)
+                    .put("message", s"Draft version of the content with id : ${node.getIdentifier} is discarded")
+                }
+            }
         })recoverWith { case e: CompletionException => throw e.getCause }
     }
 
@@ -59,5 +67,66 @@ object DiscardManager {
         oec.graphService.deleteExternalProps(request).map(resp => DataNode.deleteNode(request)).flatMap(f => f)
     }
 
+    def cleanLanguageReferences(discardedId: String)(implicit ec: ExecutionContext, oec: OntologyEngineContext): Future[Unit] = {
+        val readReq = new Request()
+        readReq.put("graph_id", "domain")
+        readReq.put("version", "1.0")
+        readReq.put("objectType", "Content")
+        readReq.put("schemaName", "content")
+        readReq.put("identifier", discardedId)
+        readReq.put("mode", "edit")
 
+        DataNode.read(readReq).flatMap { discardedNode =>
+            val languageMap = discardedNode.getMetadata.getOrDefault("languageMapV1", new util.HashMap[String, AnyRef]()).asInstanceOf[util.Map[String, AnyRef]]
+            val targets = languageMap.values().toList.collect {
+            case v: util.Map[_, _] if v.asInstanceOf[util.Map[String, AnyRef]].get("id") != discardedId =>
+                v.asInstanceOf[util.Map[String, AnyRef]].get("id").asInstanceOf[String]
+            }
+
+            val discardLangOpt = languageMap.entrySet().find(e => {
+            val value = e.getValue
+            value match {
+                case map: util.Map[_, _] =>
+                map.asInstanceOf[util.Map[String, AnyRef]].get("id") == discardedId
+                case _ => false
+            }
+            }).map(_.getKey)
+
+            discardLangOpt match {
+                case Some(discardLang) =>
+                    val updateFutures = targets.map { id =>
+                    val readNodeReq = new Request()
+                    readNodeReq.put("graph_id", "domain")
+                    readNodeReq.put("version", "1.0")
+                    readNodeReq.put("objectType", "Content")
+                    readNodeReq.put("schemaName", "content")
+                    readNodeReq.put("identifier", id)
+                    readNodeReq.put("mode", "edit")
+
+                    DataNode.read(readNodeReq).flatMap { node =>
+                        val versionKey = node.getMetadata.getOrDefault("versionKey", "").asInstanceOf[String]
+                        val langMap = node.getMetadata.get("languageMapV1").asInstanceOf[util.Map[String, AnyRef]]
+                        langMap.remove(discardLang)
+
+                        val updateReq = new Request()
+                        updateReq.setOperation("systemUpdate")
+                        updateReq.setRequest(new util.HashMap[String, AnyRef]() {{
+                        put("languageMapV1", langMap)
+                        put("versionKey", versionKey)
+                        }})
+                        updateReq.setContext(new util.HashMap[String, AnyRef]() {{
+                        put("graph_id", "domain")
+                        put("version", "1.0")
+                        put("objectType", "Content")
+                        put("schemaName", "content")
+                        put("identifier", id)
+                        }})
+                        DataNode.update(updateReq).map(_ => ())
+                    }
+                    }
+                    Future.sequence(updateFutures).map(_ => ())
+                case None => Future.successful(())
+            }
+        }
+    }
 }
