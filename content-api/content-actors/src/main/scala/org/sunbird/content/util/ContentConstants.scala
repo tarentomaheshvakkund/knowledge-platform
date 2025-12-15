7d9939f2fb1c2281fa1482c7544db96b00ffedd4
Pathik Nandi <86921116+Pathik-Nandi@users.noreply.github.com>
2025-12-15 18:19:23 +0530

    KB-12295 Adding validation for multilingual for retire the courses. If any course is multilingual then we can not retired that course. (#145)
    * KB-12296 Adding if name is present in request then it will take others wise it will take from old data course.
    
    * KB-12295 Adding validation for multilingual for retire the courses. If any course is multilingual then we can not retired that course.
    
    * KB-12295 Adding validation for multilingual for retire the courses. If any course is multilingual then we can not retired that course.
    
    * KB-12295 Adding validation for multilingual for retire the courses. If any course is multilingual then we can not retired that course.
    
    ---------
    
    Co-authored-by: pathiknandi <pathik.nandi@tarento.com>

--- content-api/content-actors/src/main/scala/org/sunbird/content/util/ContentConstants.scala
+++ content-api/content-actors/src/main/scala/org/sunbird/content/util/ContentConstants.scala
@@ -152,7 +152,11 @@ object ContentConstants {
     val SUNBIRD__KEYSPACE = "cbplan.lookup.keyspace"
     val CB_PLAN_LOOKUP_TABLE = "cbplan.lookup.table"
     val SUCCESS = "successful"
-    val ISVALID = "isValid"
+    val IS_VALID = "isValid"
     val MESSAGES = "messages"
     val FAILED = "Failed"
+    val LANGUAGE_MAP_V1 = "languageMapV1"
+    val ERR_ACTIVE_MULTILINGUAL_COURSE = "ERR_ACTIVE_MULTILINGUAL_COURSE"
+    val ERR_ACTIVE_MULTILINGUAL_COURSE_MSG = "Cannot retire any course that belongs to a multilingual course with active language versions."
+    val IS_BASE_LANG = "isBaseLang"
 }
--- content-api/content-actors/src/main/scala/org/sunbird/content/util/RetireManager.scala
+++ content-api/content-actors/src/main/scala/org/sunbird/content/util/RetireManager.scala
@@ -34,7 +34,7 @@ import java.time.{Instant, ZoneOffset}
 import java.time.format.{DateTimeFormatter, DateTimeParseException}
 import java.time.temporal.ChronoUnit
 import scala.collection.JavaConversions._
-import scala.collection.JavaConverters.asScalaBufferConverter
+import scala.collection.JavaConverters.{asScalaBufferConverter, collectionAsScalaIterableConverter}
 import scala.collection.mutable.ListBuffer
 import scala.concurrent.{ExecutionContext, Future, Promise}
 
@@ -93,6 +93,7 @@ object RetireManager {
       for {
         _ <- validateNoParentCollection(request)
         _ <- validateNoCbPlanForContent(contentId)
+        _ <- validateMultilingualRetirement(contentId)
         _ <- insertRetirementRequest(contentId, reqMap)
         resp <- markContentPendingRetirement(request)
       } yield resp
@@ -144,7 +145,7 @@ object RetireManager {
       params.setStatus(ContentConstants.SUCCESS)
       params.setErrmsg(null)
       resp.put(ContentConstants.IDENTIFIER, contentId)
-      resp.put(ContentConstants.ISVALID, java.lang.Boolean.TRUE)
+      resp.put(ContentConstants.IS_VALID, java.lang.Boolean.TRUE)
       resp.put(ContentConstants.MESSAGES, null)
       resp
     }.recover {
@@ -156,7 +157,7 @@ object RetireManager {
         params.setErrmsg(clientException.getMessage)
 
         resp.put(ContentConstants.IDENTIFIER, contentId)
-        resp.put(ContentConstants.ISVALID, java.lang.Boolean.FALSE)
+        resp.put(ContentConstants.IS_VALID, java.lang.Boolean.FALSE)
         resp.put(ContentConstants.MESSAGES, clientException.getMessage)
         resp
 
@@ -578,5 +579,61 @@ object RetireManager {
     }
 
     private def getLearningGraphEvent(request: Request, id: String): Map[String, Any] = Map("ets" -> System.currentTimeMillis(), "channel" -> request.getContext.get(ContentConstants.CHANNEL), "mid" -> UUID.randomUUID.toString, "nodeType" -> "DATA_NODE", "userId" -> "Ekstep", "createdOn" -> DateUtils.format(new Date()), "objectType" -> "Content", "nodeUniqueId" -> id, "operationType" -> "DELETE", "graphId" -> request.getContext.get("graph_id"))
+
+    private def validateMultilingualRetirement(contentId: String)
+                                              (implicit ec: ExecutionContext,
+                                               oec: OntologyEngineContext): Future[Unit] = {
+      val readReq = new Request()
+      readReq.setContext(new java.util.HashMap[String, AnyRef]() {
+        {
+          put("graph_id", "domain")
+          put("version", ContentConstants.SCHEMA_VERSION)
+          put("objectType", ContentConstants.CONTENT_OBJECT_TYPE)
+          put("schemaName", ContentConstants.CONTENT_SCHEMA_NAME)
+        }
+      })
+      readReq.put(ContentConstants.IDENTIFIER, contentId)
+      readReq.put(ContentConstants.MODE, "read")
+
+      DataNode.read(readReq).map { node =>
+        val metadata = Option(node.getMetadata)
+          .getOrElse(throw new ClientException(
+            ContentConstants.ERR_INVALID_CONTENT,
+            "Content metadata is missing."
+          ))
+
+        val langMapOpt = Option(metadata.get(ContentConstants.LANGUAGE_MAP_V1))
+          .map(_.asInstanceOf[java.util.Map[String, java.util.Map[String, AnyRef]]])
+
+        if (langMapOpt.isDefined) {
+          val langMap = langMapOpt.get
+          val entries = langMap.values().asScala.toList
+          val retiringEntryOpt = entries.find(e => Option(e.get("id")).map(_.toString) == Some(contentId))
+          retiringEntryOpt match {
+            case Some(retiringEntry) =>
+              val isBase = Option(retiringEntry.get(ContentConstants.IS_BASE_LANG)).exists(_.toString.toBoolean)
+              if (isBase && entries.size > 1) {
+                // Check if all non-base language versions are retired
+                val nonBaseEntries = entries.filterNot(_ == retiringEntry)
+                val allNonBaseRetired = nonBaseEntries.forall(e =>
+                  Option(e.get(ContentConstants.STATUS)).exists(_.toString.equalsIgnoreCase(ContentConstants.RETIRED))
+                )
+                if (!allNonBaseRetired) {
+                  throw new ClientException(
+                    ContentConstants.ERR_ACTIVE_MULTILINGUAL_COURSE,
+                    ContentConstants.ERR_ACTIVE_MULTILINGUAL_COURSE_MSG
+                  )
+                }
+              } else if (!isBase) {
+                logger.info(s"Retiring non-base language content: $contentId")
+              }
+            case None =>
+              logger.warn(s"Content $contentId not found in language map during multilingual validation, allowing retirement")
+              ()
+          }
+        }
+      }
+    }
+
 
 }
